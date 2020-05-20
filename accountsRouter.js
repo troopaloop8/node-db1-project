979ab8e233ce5c8538e9d74be65218ca9b31e138
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-05-20 15:18:38 +0900

    init, sql query questions done, progress on endpoints

---
+++ accountsRouter.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./data/dbConfig.js');
+
+
+//CRUD STUFF
+//GET
+
+router.get('/', (req, res) => {
+    db('accounts')
+    .then(post => {
+        if (post.length === 0){
+            res.status(404).json({message: "no accounts exist..."})
+        } else {
+        res.status(200).json(post);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: `Something went wrong... ${err}`})
+    })
+})
+
+
+//GET BY ID
+
+router.get('/:id', ({params: {id}}, res) => {
+    db('accounts').where({id})
+    .then(post => {
+        if (post.length === 0) {
+            res.status(404).json({message: "invalid id"})
+        } else {
+            res.status(200).json(post);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: `Something went wrong... ${err}`})
+    })
+})
+
+
+//POST
+
+router.post('/', (req, res) => {
+    db('accounts').insert(req.body)
+    .then(post => {
+        res.status(201).json({message: `successfully added account`}, post)
+    })
+    .catch(err => {
+        res.status(500).json({message: `Something went wrong... ${err}`})
+    })
+})
+
+
+
+//PUT
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    db('accounts').where({id}).update(req.body)
+    .then(post => {
+        if (post.length === 0) {
+            res.status(404).json({message: `invalid id`})
+        } else {
+            res.status(201).json({message: `successfully updated`}, post)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: `Something went wrong... ${err}`})
+    })
+})
+
+
+
+
+//DELETE
+
+router.delete('/:id', ({params: {id}}, res) => {
+    db('accounts').where({id}).del()
+    .then(post => {
+        if (post.length === 0) {
+            res.status(404).json({message: `invalid id`})
+        } else {
+            res.status(200).json({message: `successfully deleted`}, post)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: `Something went wrong... ${err}`})
+    })
+})
+
+
+
+//EXPORTS / MIDDLEWARE
+
+module.exports = router;
